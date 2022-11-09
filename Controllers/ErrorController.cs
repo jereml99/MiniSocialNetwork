4be343d8576ee9d1fed455be7445aa6f11efe57c
grepoz <grzegorz.pozorski@gmail.com>
2022-11-09 15:56:46 +0100

    finished

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniSocialNetwork.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Error");
+        }
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View("NotFound");
+        }
+    }
+}
--- Controllers/FriendController.cs
+++ Controllers/FriendController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MiniSocialNetwork.Models;
+using System.Data;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace MiniSocialNetwork.Controllers
 {
@@ -75,13 +80,53 @@ namespace MiniSocialNetwork.Controllers
         public ActionResult Export()
         {
             if (!hasAccess()) return RedirectToAction("Login", "Login");
-            var friends = Globals.CurrentlyLoggedUser?.Friends;
-            if (friends is null || friends.Count == 0){
 
+            var friends = Globals.CurrentlyLoggedUser?.Friends;
+
+            if (friends is null || friends.Count == 0){
+                return View("List", Globals.CurrentlyLoggedUser);
             }
 
-            //System.IO.File.WriteAllLines("SavedLists.txt", );
-            return View(Globals.CurrentlyLoggedUser);
+            string filePath = $"friends_of_{Globals.CurrentlyLoggedUser!.Login}_" + DateTime.Today.ToString("yyyy-MM-dd") + ".txt";
+
+            StringBuilder sb = new();
+            Globals.CurrentlyLoggedUser!.Friends.ForEach(f => sb.AppendLine(f));
+            byte[] fileContent = Encoding.ASCII.GetBytes(sb.ToString());
+
+            return new FileContentResult(fileContent, "text/txt")
+            {
+                FileDownloadName = filePath
+            };
+        }
+
+        public ActionResult Import()
+        {
+            if (!hasAccess()) return RedirectToAction("Login", "Login");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Import(IFormFile postedFile)
+        {
+            Globals.CurrentlyLoggedUser?.Friends.Clear();
+            Globals.CurrentlyLoggedUser?.Friends.AddRange(RetriveFriendsFromFile(postedFile));
+
+            return View("List", Globals.CurrentlyLoggedUser);
+        }
+
+        private List<string> RetriveFriendsFromFile(IFormFile file)
+        {
+            var result = new List<string>();
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    result.Add(reader.ReadLine() ?? "");
+                }
+            }
+
+            return result;
         }
 
         private bool hasAccess() => Globals.CurrentlyLoggedUser != null;
